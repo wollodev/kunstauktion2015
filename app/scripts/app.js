bb1fa423a332898dc59419e0e9ef1c14bf71dd8d
Wolfgang Kluth <me@kluth.co.uk>
2015-02-15 22:07:20 +0100

    updated layout and added lightbox

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,9 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'ngTooltip',
+    'bootstrapLightbox'
   ])
   .config(function ($routeProvider) {
     $routeProvider
@@ -30,4 +32,44 @@ angular
       .otherwise({
         redirectTo: '/'
       });
+  })
+  .config(function (LightboxProvider) {
+    // set a custom template
+    LightboxProvider.templateUrl = 'views/lightbox-modal.html';
+
+    // our images array is not in the default format, so we have to write this
+    // custom method
+    LightboxProvider.getImageUrl = function (imageUrl) {
+      return imageUrl;
+    };
+
+    // set the caption of each image as its text color
+    LightboxProvider.getImageCaption = function (imageUrl) {
+      return '#' + imageUrl.match(/00\/(\w+)/)[1];
+    };
+
+    //// increase the maximum display height of the image
+    //LightboxProvider.calculateImageDimensionLimits = function (dimensions) {
+    //  return {
+    //    'maxWidth': dimensions.windowWidth >= 768 ? // default
+    //    dimensions.windowWidth - 92 :
+    //    dimensions.windowWidth - 52,
+    //    'maxHeight': 1600                           // custom
+    //  };
+    //};
+
+    // the modal height calculation has to be changed since our custom template is
+    // taller than the default template
+    LightboxProvider.calculateModalDimensions = function (dimensions) {
+      var width = Math.max(300, dimensions.imageDisplayWidth + 32);
+
+      if (width >= dimensions.windowWidth - 20 || dimensions.windowWidth < 768) {
+        width = 'auto';
+      }
+
+      return {
+        'width': width,                             // default
+        'height': 'auto'                            // custom
+      };
+    };
   });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,17 @@
  * Controller of the kunstauktion2015App
  */
 angular.module('kunstauktion2015App')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+  .controller('MainCtrl', function ($scope, Lightbox) {
+
+    $scope.Lightbox = Lightbox;
+
+    $scope.images = [
+      'http://dummyimage.com/100x100/8ac4f6/ffffff.png',
+      'http://dummyimage.com/200x200/29ed98/ffffff.png',
+      'http://dummyimage.com/300x300/c47ee9/ffffff.png',
+      'http://dummyimage.com/400x400/716e5c/ffffff.png',
+      'http://dummyimage.com/500x500/b37752/ffffff.png',
+      'http://dummyimage.com/600x600/20b7ea/ffffff.png',
+      'http://dummyimage.com/700x700/586163/ffffff.png'
     ];
   });
